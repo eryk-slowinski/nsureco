79bcb7137258a993f415ff10ef9de67558d3c79d
eryk-slowinski <e.slowinski92@gmail.com>
2021-08-11 13:38:20 +0200

    moved all imports to main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { appRoutingModule, routingComponents } from './appRoutingModule';
-import { UserLoginModule } from './components/user-login/user-login.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { appRoutingModule } from './appRoutingModule';
 import { HttpClientModule} from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+//Components
+import { AppComponent } from './app.component';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+import { CreatePolicyComponent } from './components/create-policy/create-policy.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
+    TopMenuComponent,
+    CreatePolicyComponent,
+    UserLoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormsModule,
     appRoutingModule,
-    UserLoginModule,
   ],
   exports: [],
   providers: [],
