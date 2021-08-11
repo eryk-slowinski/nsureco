6456e2cee3e32d70dfb00b13d95d7b38b7dccf31
eryk-slowinski <e.slowinski92@gmail.com>
2021-08-11 13:39:04 +0200

    removed logic from components to service

--- src/app/appRoutingModule.ts
+++ src/app/appRoutingModule.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import {Routes, RouterModule} from '@angular/router';
+import { CreatePolicyComponent } from "./components/create-policy/create-policy.component";
 import { UserLoginComponent } from "./components/user-login/user-login.component";
 import { UserPanelComponent } from "./components/user-panel/user-panel.component";
 
 const routes: Routes = [
     { path: 'userlogin', component: UserLoginComponent},
-    { path: 'userpanel', component: UserPanelComponent}
+    { path: 'userpanel', component: UserPanelComponent},
+    {path: 'createpolicy', component: CreatePolicyComponent}
 ];
 
 @NgModule({
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,12 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  userName: string = '';
+  constructor(private http: HttpClient, private router: Router) { }
 
-  constructor() { }
+  url: string = `http://localhost:8082/verify`;
+  userCredentials: Object = new Object();
+  userName: string = '';
+  userPassword: string = '';
+  loggedUser = new Subject<string>();
+
+  getUserData(userName: string, userPassword: string): Object {  //getting users inputs from html form
+    this.userName = userName;
+    this.userPassword = userPassword;
+    return this.userCredentials = {
+      userName: this.userName,
+      userPassword: this.userPassword
+    };
+  }
+
+  async postUser(): Promise<Object> {
+    return await this.http.post<any>(this.url, this.userCredentials).toPromise();
+  }
+
+  verifyUser(userObj: Object) {
+    if (userObj['name'] === 'NOT_EXIST') {
+      return '';
+    } else {
+      this.router.navigate(['/'], { queryParams: { loggedin: 'true' } });
+      this.loggedUser.next(userObj['name']);
+      return userObj['name'];
+    }
+  }
+
+  userLogIn(): Observable<string> {
+    return this.loggedUser.asObservable();
+  }
 
 }
