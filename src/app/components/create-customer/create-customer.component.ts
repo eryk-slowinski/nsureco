2bc793f41c660c26c9b6f902d997e2f35cb3edc9
eryk-slowinski <e.slowinski92@gmail.com>
2021-09-12 19:27:02 +0200

    Created components: Search customer and create customer

---
+++ src/app/components/create-customer/create-customer.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomerService } from 'src/app/services/customer.service';
+
+@Component({
+  selector: 'app-create-customer',
+  templateUrl: './create-customer.component.html'
+})
+export class CreateCustomerComponent implements OnInit {
+
+  constructor(private customerService: CustomerService) { }
+
+  createCustomerForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    birth_date: new FormControl(null, Validators.required),
+    address: new FormControl(null, Validators.required),
+    pesel: new FormControl(null, Validators.required),
+    phone_num: new FormControl(null, Validators.required)
+  });
+
+onSubmit(createCustomerForm: FormGroup) {
+  console.log(createCustomerForm.value);
+  this.customerService.addCustomer(createCustomerForm);
+}
+
+get name() {
+  return this.createCustomerForm.get('name');
+}
+
+get birth_date() {
+  return this.createCustomerForm.get('birth_date');
+}
+
+get address() {
+  return this.createCustomerForm.get('address');
+}
+
+get pesel() {
+  return this.createCustomerForm.get('pesel');
+}
+
+get phone_num() {
+  return this.createCustomerForm.get('phone_num');
+}
+  ngOnInit(): void {
+  }
+
+}
