eccee997d5beb5dd5d973d56fa90e69c3d557147
eryk-slowinski <e.slowinski92@gmail.com>
2021-09-21 20:44:13 +0200

    Created Behavior subject in customer service, so it is possible to pass selected customer from customer list to another independent component (for example policy component) Selecting customer from list is updating  value of selected customer in service and it is available to all components that has customerservice injected

--- src/app/modules/customers/search-customer/search-customer.component.ts
+++ src/app/modules/customers/search-customer/search-customer.component.ts
@@ -1,37 +1,28 @@
 import { CustomerService } from '../../../services/customer.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Customer } from 'src/app/models/customer';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-customer',
   templateUrl: './search-customer.component.html',
 })
-export class SearchCustomerComponent implements OnInit {
+export class SearchCustomerComponent {
   constructor(private customerService: CustomerService) {}
 
   searchResults: [];
-  // searchResultsObs: Subject<[]> = new Subject<[]>();
 
   searchForm = new FormGroup({
     searchBy: new FormControl('name', Validators.required),
     searchValue: new FormControl(null, Validators.required),
   });
 
-  // async onSubmit() {
-  //   this.searchResults = await this.customerService.searchCustomer(this.searchForm).then();
-  // }
-
-  onSubmit() {
-    // this.customerService.searchCustomer(this.searchForm).subscribe((data) => {
-    //   this.searchResults = data;
-    // })
+  async onSubmit() {
+    this.searchResults = await this.customerService
+      .searchCustomer(this.searchForm)
+      .then();
   }
 
-  // public selectCustomer(customer: Object) {
-  //   this.customerService.selectCustomer(customer);
-  // }
-
-  ngOnInit(): void {}
+  selectCustomer(customer: Object) {
+    this.customerService.customerSelected.next(customer);
+  }
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,8 +1,7 @@
-import { Subject } from 'rxjs';
-import { Customer } from './../models/customer';
 import { FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,27 +9,23 @@ import { Injectable } from '@angular/core';
 export class CustomerService {
   constructor(private http: HttpClient) {}
 
-  private selectedCustomer: Subject<any> = new Subject<any>();
   urlName: string = 'http://localhost:8082/customerSearchByName';
   urlId: string = 'http://localhost:8082/customerSearchByID';
   urlPesel: string = 'http://localhost:8082/customerSearchByPesel';
   urlAddCustomer: string = 'http://localhost:8082/addCustomer';
+  customerSelected: BehaviorSubject<Object> = new BehaviorSubject<any>(Object); //variable declared to handle customer selected between independent components
 
   setUrl(searchForm: FormGroup): string {
-    switch (searchForm.value.searchBy) {
-      case 'name': {
-        return this.urlName;
-      }
-      case 'pesel': {
-        return this.urlPesel;
-      }
-      default: {
-        return this.urlId;
-      }
+    if (searchForm.value.searchBy == 'name') {
+      return this.urlName;
+    } else if (searchForm.value.searchBy == 'pesel') {
+      return this.urlPesel;
+    } else {
+      return this.urlId;
     }
   }
 
-  async searchCustomer(searchForm: FormGroup): Promise<Customer> {
+  async searchCustomer(searchForm: FormGroup): Promise<any> {
     let url = this.setUrl(searchForm);
     let searchJson = {
       [searchForm.value.searchBy]: searchForm.value.searchValue,
@@ -42,13 +37,5 @@ export class CustomerService {
     return await this.http
       .post<any>(this.urlAddCustomer, createCustomerForm.value)
       .toPromise();
-  }
-
-  public selectCustomer(customer: Object) {
-    this.selectedCustomer.next(customer);
-  }
-
-  public getSelectedCustomer() {
-    return this.selectedCustomer.asObservable();
   }
 }
