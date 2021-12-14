cd9109224f301be5467bd9a85eca09ae13533a92
eryk-slowinski <e.slowinski92@gmail.com>
2021-12-14 11:43:00 +0100

    changes in objectRisks model

--- src/app/models/objectRisks.ts
+++ src/app/models/objectRisks.ts
@@ -2,6 +2,8 @@ export class ObjectRisks {
   id: number;
   riskId: string;
   objectId: number;
+  premium: number;
   premiumForPeriod: number;
   isSelected: string;
+  checkboxValue: boolean;
 }
