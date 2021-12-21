a8b6b470aa5f523c8c28f3813a441b706ca9f381
eryk-slowinski <e.slowinski92@gmail.com>
2021-12-21 18:15:28 +0100

    Recent changes in edit policy component, after selecting a policy to edit user is redirected to screen where whole data that can be changed is displayed and under each table is button to modify. Functionality behind buttons is going be added in next commits.

--- src/app/modules/policies/edit-policy/edit-policy.component.ts
+++ src/app/modules/policies/edit-policy/edit-policy.component.ts
@@ -1,6 +1,10 @@
+import { Vehicle } from './../../../models/vehicle';
+import { InsuredObject } from './../../../models/insuredObject';
 import { CustomerService } from './../../../services/customer.service';
 import { PolicyService } from 'src/app/services/policy.service';
 import { Component, OnInit } from '@angular/core';
+import { Policy } from 'src/app/models/policy';
+import { PolicyLine } from 'src/app/models/policyLine';
 
 @Component({
   selector: 'app-edit-policy',
@@ -12,6 +16,50 @@ export class EditPolicyComponent implements OnInit {
   constructor(private policyService: PolicyService, private customerService: CustomerService) { }
   policySelected: Object = new Object();
   customerSelected: Object = new Object();
+  policyLine: PolicyLine = new PolicyLine();
+  policy: Policy = new Policy();
+  insuredVehicle: InsuredObject = new InsuredObject();
+  insuredDriver: InsuredObject = new InsuredObject();
+  vehicle: Vehicle = new Vehicle();
+
+  async getPolicy() {
+    this.policy = await this.policyService.getPolicy(this.policySelected).then();
+  }
+
+  async getPolicyLine() {
+    this.policyLine.policyId = this.policySelected['policyId'];
+    this.policyLine.transactionId = this.policySelected['transactionId'];
+    this.policyLine = await this.policyService.getPolicyLine(this.policyLine).then();
+  }
+
+  async searchInsuredObjects() {
+    this.insuredVehicle.policyLineId = this.policyLine.policyLineId;
+    this.insuredVehicle.type = 'VEH';
+    this.insuredVehicle = await this.policyService.searchInsuredObject(this.insuredVehicle).then();
+    this.insuredDriver.policyLineId = this.policyLine.policyLineId;
+    this.insuredDriver.type = 'DRI';
+    this.insuredDriver = await this.policyService.searchInsuredObject(this.insuredDriver).then();
+  }
+
+  async searchVehicle() {
+    this.vehicle.vehicleId = this.insuredVehicle.n01;
+    this.vehicle = await this.policyService.searchVehicle(this.vehicle).then();
+  }
+
+  async getAllData() {
+    await this.getPolicy();
+    await this.getPolicyLine();
+    await this.searchInsuredObjects();
+    await this.searchVehicle();
+  }
+
+
+
+
+  async updatePolicy() {
+    this.policy.type = 'Quote';
+    await this.policyService.updatePolicy(this.policy).then();
+  }
 
   ngOnInit(): void {
     this.policyService.policySelected.subscribe((policy) => {
@@ -20,6 +68,6 @@ export class EditPolicyComponent implements OnInit {
     this.customerService.customerSelected.subscribe((customer) => {
       this.customerSelected = customer;
     });
+    this.getAllData();
   }
-
 }
