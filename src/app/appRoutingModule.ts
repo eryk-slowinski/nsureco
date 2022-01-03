c026b5fe6c58820ba4f3a64a86cb7ea85af5dcc3
eryk-slowinski <e.slowinski92@gmail.com>
2022-01-03 12:37:08 +0100

    Removed search policy component and all imports/links to it

--- src/app/appRoutingModule.ts
+++ src/app/appRoutingModule.ts
@@ -4,7 +4,6 @@ import { CreateCustomerComponent } from './modules/customers/create-customer/cre
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreatePolicyComponent } from './modules/policies/create-policy/create-policy.component';
-import { SearchPolicyComponent } from './modules/policies/search-policy/search-policy.component';
 import { UserLoginComponent } from './modules/users/user-login/user-login.component';
 import { HomeComponent } from './modules/core/home/home.component';
 import { CustomerComponent } from './modules/customers/customer/customer.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
   { path: 'userlogin', component: UserLoginComponent },
   //COMMENTED FOR TESTING PURPOSES
   // { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  // { path: 'searchpolicy', component: SearchPolicyComponent, canActivate: [AuthGuard] },
   // { path: 'createpolicy', component: CreatePolicyComponent, canActivate: [AuthGuard] },
   // { path: 'editpolicy', component: EditPolicyComponent, canActivate: [AuthGuard] },
   // { path: 'createcustomer', component: CreateCustomerComponent, canActivate: [AuthGuard] },
@@ -22,7 +20,6 @@ const routes: Routes = [
   // { path: 'customer', component: CustomerComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent },
   { path: 'createpolicy', component: CreatePolicyComponent },
-  { path: 'searchpolicy', component: SearchPolicyComponent },
   { path: 'editpolicy', component: EditPolicyComponent },
   { path: 'createcustomer', component: CreateCustomerComponent },
   { path: 'searchcustomer', component: SearchCustomerComponent },
--- src/app/modules/policies/policies.module.ts
+++ src/app/modules/policies/policies.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreatePolicyComponent } from './create-policy/create-policy.component';
 import { SharedModule } from '../shared/shared.module';
-import { SearchPolicyComponent } from './search-policy/search-policy.component';
 import { EditPolicyComponent } from './edit-policy/edit-policy.component';
 
 @NgModule({
-  declarations: [CreatePolicyComponent, SearchPolicyComponent, EditPolicyComponent],
+  declarations: [CreatePolicyComponent, EditPolicyComponent],
   exports: [CreatePolicyComponent],
   imports: [CommonModule, SharedModule],
 })
-export class PoliciesModule {}
+export class PoliciesModule { }
