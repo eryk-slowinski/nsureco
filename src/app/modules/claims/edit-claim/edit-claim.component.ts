5e39593c344332474bc949605d2db70658687f4e
eryk-slowinski <e.slowinski92@gmail.com>
2022-01-06 18:41:57 +0100

    Fixing heroku development

--- src/app/modules/claims/edit-claim/edit-claim.component.ts
+++ src/app/modules/claims/edit-claim/edit-claim.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-edit-claim',
-  templateUrl: './edit-claim.component.html',
-  styleUrls: ['./edit-claim.component.css']
+  templateUrl: './edit-claim.component.html'
 })
 export class EditClaimComponent implements OnInit {
 
